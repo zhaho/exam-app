b7b030b9996cae1dc90d8de098b91ae3b1beec14
kenneth <zhaho.ros@gmail.com>
2025-02-23 10:43:52 +0000

    fix: remote ui button

--- frontend/src/components/ButtonCopyToRemNote.jsx
+++ frontend/src/components/ButtonCopyToRemNote.jsx
@@ -16,32 +16,36 @@ const ButtonCopyToRemNote = ({ copyText, buttonText }) => {
         document.execCommand("copy");
         document.body.removeChild(textArea);
       }
-  
+
       setCopied(true);
       setTimeout(() => setCopied(false), 2000);
     } catch (error) {
       console.error("Failed to copy text:", error);
     }
   };
-  
-  
+
+
 
   return (
     <button
+      className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
       onClick={handleCopy}
-      className="flex items-center gap-2 px-4 py-2 bg-purple-600 text-white font-medium rounded-lg shadow-sm hover:bg-purple-700 transition-all duration-300">
-      {copied ? (
-        <>
-          <CheckCircle className="w-5 h-5 text-green-400" />
-          Copied!
-        </>
-      ) : (
-        <>
-          <Clipboard className="w-5 h-5" />
-          {buttonText}
-        </>
-      )}
+    >
+      <span className="relative flex items-center gap-2 px-10 py-1 transition-all ease-in duration-75 bg-white rounded-md group-hover:bg-transparent text-black hover:text-white">
+        {copied ? (
+          <>
+            <CheckCircle className="w-5 h-5 text-green-400" />
+            Copied!
+          </>
+        ) : (
+          <>
+            <Clipboard className="w-5 h-5" />
+            {buttonText}
+          </>
+        )}
+      </span>
     </button>
+
   );
 };
 
